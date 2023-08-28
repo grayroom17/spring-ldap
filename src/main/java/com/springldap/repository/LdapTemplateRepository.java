36a89245589f4fa2b2b05c3d9a132a4fbdd51277
DeevS <deev@advantum.pro>
2023-08-28 19:57:27 +0300

    Update ways: rebind(unbind and then bind) ldap entry, LdapTemplate.modifyAttributes()

--- src/main/java/com/springldap/repository/LdapTemplateRepository.java
+++ src/main/java/com/springldap/repository/LdapTemplateRepository.java
@@ -1,6 +1,7 @@
 package com.springldap.repository;
 
 import com.springldap.domain.entity.LdapUser;
+import com.springldap.rest.dto.AttributeDto;
 import com.springldap.rest.dto.UserCreateDto;
 
 import java.util.List;
@@ -19,5 +20,9 @@ public interface LdapTemplateRepository {
     void create(UserCreateDto dto);
 
     void delete(String dn);
+
+    void rebind(String dn, UserCreateDto dto);
+
+    void updateAttribute(String dn, AttributeDto attribute);
 
 }
