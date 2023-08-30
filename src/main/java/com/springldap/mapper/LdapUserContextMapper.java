f6f2e77c503cddd81af02cfdb54cd802e9d4696c
DeevS <deev@advantum.pro>
2023-08-30 20:45:18 +0300

    convenience AbstractContextMapper instead ContextMapper

--- src/main/java/com/springldap/mapper/LdapUserContextMapper.java
+++ src/main/java/com/springldap/mapper/LdapUserContextMapper.java
@@ -1,53 +1,52 @@
 package com.springldap.mapper;
 
 import com.springldap.domain.entity.LdapUser;
-import org.springframework.ldap.core.ContextMapper;
-import org.springframework.ldap.core.DirContextAdapter;
+import org.springframework.ldap.core.DirContextOperations;
+import org.springframework.ldap.core.support.AbstractContextMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component
-public class LdapUserContextMapper implements ContextMapper<LdapUser> {
+public class LdapUserContextMapper extends AbstractContextMapper<LdapUser> {
 
     @Override
-    public LdapUser mapFromContext(Object ctx) {
-        DirContextAdapter context = (DirContextAdapter) ctx;
+    public LdapUser doMapFromContext(DirContextOperations ctx) {
         return LdapUser.builder()
-                .distinguishedName(context.getDn())
-                .commonName(context.getStringAttribute("cn"))
-                .canonicalName(context.getStringAttribute("canonicalName"))
-                .guid(context.getStringAttribute("objectGuid"))
-                .userPrincipalName(context.getStringAttribute("userPrincipalName"))
-                .displayName(context.getStringAttribute("displayName"))
-                .fullName(context.getStringAttribute("Name"))
-                .firstname(context.getStringAttribute("GivenName"))
-                .lastname(context.getStringAttribute("sn"))
-                .otherName(context.getStringAttribute("OtherName"))
-                .initials(context.getStringAttribute("initials"))
-                .telephoneNumber(context.getStringAttribute("telephoneNumber"))
-                .homePhone(Arrays.asList(context.getStringAttributes("homePhone")))
-                .mobilePhone(context.getStringAttribute("mobilePhone"))
-                .country(context.getStringAttribute("country"))
-                .state(context.getStringAttribute("state"))
-                .city(context.getStringAttribute("city"))
-                .street(context.getStringAttribute("street"))
-                .postalCode(context.getStringAttribute("postalCode"))
-                .company(context.getStringAttribute("company"))
-                .organization(context.getStringAttribute("organization"))
-                .division(context.getStringAttribute("division"))
-                .department(context.getStringAttribute("department"))
-                .office(context.getStringAttribute("office"))
-                .manager(context.getStringAttribute("manager"))
-                .employeeId(context.getStringAttribute("employeeId"))
-                .employeeNumber(context.getStringAttribute("employeeNumber"))
-                .mail(context.getStringAttribute("mail"))
-                .mailNickname(context.getStringAttribute("mailNickname"))
-                .samAccountName(context.getStringAttribute("samAccountName"))
-                .officePhone(context.getStringAttribute("officePhone"))
-                .ipPhone(context.getStringAttribute("ipPhone"))
-                .title(context.getStringAttribute("title"))
-                .enabled(context.getStringAttribute("enabled"))
+                .distinguishedName(ctx.getDn())
+                .commonName(ctx.getStringAttribute("cn"))
+                .canonicalName(ctx.getStringAttribute("canonicalName"))
+                .guid(ctx.getStringAttribute("objectGuid"))
+                .userPrincipalName(ctx.getStringAttribute("userPrincipalName"))
+                .displayName(ctx.getStringAttribute("displayName"))
+                .fullName(ctx.getStringAttribute("Name"))
+                .firstname(ctx.getStringAttribute("GivenName"))
+                .lastname(ctx.getStringAttribute("sn"))
+                .otherName(ctx.getStringAttribute("OtherName"))
+                .initials(ctx.getStringAttribute("initials"))
+                .telephoneNumber(ctx.getStringAttribute("telephoneNumber"))
+                .homePhone(Arrays.asList(ctx.getStringAttributes("homePhone")))
+                .mobilePhone(ctx.getStringAttribute("mobilePhone"))
+                .country(ctx.getStringAttribute("country"))
+                .state(ctx.getStringAttribute("state"))
+                .city(ctx.getStringAttribute("city"))
+                .street(ctx.getStringAttribute("street"))
+                .postalCode(ctx.getStringAttribute("postalCode"))
+                .company(ctx.getStringAttribute("company"))
+                .organization(ctx.getStringAttribute("organization"))
+                .division(ctx.getStringAttribute("division"))
+                .department(ctx.getStringAttribute("department"))
+                .office(ctx.getStringAttribute("office"))
+                .manager(ctx.getStringAttribute("manager"))
+                .employeeId(ctx.getStringAttribute("employeeId"))
+                .employeeNumber(ctx.getStringAttribute("employeeNumber"))
+                .mail(ctx.getStringAttribute("mail"))
+                .mailNickname(ctx.getStringAttribute("mailNickname"))
+                .samAccountName(ctx.getStringAttribute("samAccountName"))
+                .officePhone(ctx.getStringAttribute("officePhone"))
+                .ipPhone(ctx.getStringAttribute("ipPhone"))
+                .title(ctx.getStringAttribute("title"))
+                .enabled(ctx.getStringAttribute("enabled"))
                 .build();
     }
 
